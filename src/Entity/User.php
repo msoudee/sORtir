2386497899dee084d8e6219c3f77ad72eff8acc9
AmOrI <AmOrI>
2019-08-20 15:00:32 +0200

    LOGIN + REGISTER FONCTIONELLEUH

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,10 +3,13 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Console\Logger\ConsoleLogger;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ * @UniqueEntity(fields={"username"}, message="There is already an account with this username")
  */
 class User implements UserInterface
 {
@@ -85,7 +88,7 @@ class User implements UserInterface
     public function setPassword(string $password): self
     {
         $this->password = $password;
-
+        echo  $this->password;
         return $this;
     }
 
