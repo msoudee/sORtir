849af3c07eace2e65bdb752d038a826214a16276
AmOrI <AmOrI>
2019-08-29 14:31:07 +0200

    Fix import csv

--- src/Controller/CSVReaderController.php
+++ src/Controller/CSVReaderController.php
@@ -23,13 +23,50 @@ class CSVReaderController extends AbstractController
         // Check if image file is a actual image or fake image
         if(isset($_POST["submit"])) {
             //$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-            $uploadOk = 1;
+            if($imageFileType != "csv"){
+                $this->addFlash('notice', "Sorry, only CSV files are allowed.");
+                $uploadOk = 0;
+            }
             // Check if $uploadOk is set to 0 by an error
             if ($uploadOk == 0) {
                 $this->addFlash('notice', "Sorry, your file was not uploaded.");
             // if everything is ok, try to upload file
             } else {
                 if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+                    //Open the file.
+                    $fileHandle = fopen("C:\wamp64\www\sORtir\src\Data/".$_FILES["fileToUpload"]["name"] , "r");
+                    //Loop through the CSV rows.
+                    $cpt = 0;
+                    $entityManager = $this->getDoctrine()->getManager();
+                    while (($row = fgetcsv($fileHandle, 0, ",")) !== FALSE) {
+                        if($cpt != 0 ){
+                            $test = $entityManager->getRepository(User::class )->findBy(array('username' => $row[0]));
+                            if(!$test){
+                                $user = new User();
+                                $user->setActif(1);
+                                $user->setUsername($row[0]);
+                                $user->setRoles("ROLE_USER");
+                                $user->setPassword(
+                                    $passwordEncoder->encodePassword(
+                                        $user,
+                                        $row[1]
+                                    )
+                                );
+                                $user->setMail($row[2]);
+                                $user->setNom($row[3]);
+                                $user->setPrenom($row[4]);
+                                $user->setTelephone($row[5]);
+                                $user->setSite($entityManager->find(Site::class, $row[6]));
+                                $entityManager->persist($user);
+                                $entityManager->flush();
+
+
+                                var_dump($user);
+                            }
+
+                        }
+                        $cpt++;
+                    }
                     $this->addFlash('notice', "Le fichier ". basename( $_FILES["fileToUpload"]["name"]). " a été upload.");
                 } else {
                     $this->addFlash('notice', "Sorry, there was an error uploading your file.");
@@ -39,40 +76,7 @@ class CSVReaderController extends AbstractController
 
         }
 
-        //Open the file.
-        $fileHandle = fopen("C:\wamp64\www\sORtir\src\Data/".$_FILES["fileToUpload"]["name"] , "r");
-        //Loop through the CSV rows.
-        $cpt = 0;
-        $entityManager = $this->getDoctrine()->getManager();
-        while (($row = fgetcsv($fileHandle, 0, ",")) !== FALSE) {
-            if($cpt != 0 ){
-                $test = $entityManager->getRepository(User::class )->findBy(array('username' => $row[0]));
-                if(!$test){
-                    $user = new User();
-                    $user->setActif(1);
-                    $user->setUsername($row[0]);
-                    $user->setRoles("ROLE_USER");
-                    $user->setPassword(
-                        $passwordEncoder->encodePassword(
-                            $user,
-                            $row[1]
-                        )
-                    );
-                    $user->setMail($row[2]);
-                    $user->setNom($row[3]);
-                    $user->setPrenom($row[4]);
-                    $user->setTelephone($row[5]);
-                    $user->setSite($entityManager->find(Site::class, $row[6]));
-                    $entityManager->persist($user);
-                    $entityManager->flush();
 
-
-                    var_dump($user);
-                }
-
-            }
-            $cpt++;
-        }
 
         return $this->redirectToRoute('app_users');
     }
